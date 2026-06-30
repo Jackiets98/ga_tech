837d18db7932983e206e3d8dfe30af651025bd3f
Jackiets <jackiechin9875@gmail.com>
2026-06-30 11:09:37 +0800

    text fixed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->environment('production')) {
+        $appUrl = (string) config('app.url');
+
+        if (str_starts_with($appUrl, 'https://')) {
             URL::forceScheme('https');
         }
     }
